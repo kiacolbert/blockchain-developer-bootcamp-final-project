6c729ef65c5433a1484ed59cb1e3b65f8ae2f703
kia <dont@email.com>
2021-11-15 08:33:47 -0500

    update to create nft

--- tools/contracts/Provenance.sol
+++ tools/contracts/Provenance.sol
@@ -2,18 +2,18 @@
 pragma solidity 0.8.9;
 
 import "hardhat/console.sol";
+import "tools/contracts/Nft.sol";
 
 
 contract Provenance {
     // state variables
     address[] private artists;
     address[] private owners;
-    bytes32[] private artwork;
+    bytes32[] private artworks;
 
     struct History { 
         address owner;
         uint256 date;
-        uint256 transactionId;
     }
 
     mapping (string => bytes32) public artistToArtwork;
@@ -24,11 +24,18 @@ contract Provenance {
     }
 
     function registerArtist(address artist) public view {
-
+        require(artist != address(0), "Artist can not have 0 address");
+        artists.add(artist);
     }
 
-    function setArtwork(bytes32 artHash) public {
-
+    function registerArtwork(address artist, string memory artURI) public returns (uint256 artId){
+        // and check that artowek is not registered
+        require(artists[artist], "Artist is not registered");
+        uint256 artId = createOriginalArtwork(artist, artURI);
+        artworks.add(artId);
+        // History history = new History({ owner: artist})
+        // artistToArtwork  // add artwork to artist.
+        return artId;
     }
 
     function getArtworkHistory(bytes32 artHash) public view returns (History[] memory) {
