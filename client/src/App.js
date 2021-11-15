03ab89544b9c4f640bc06304ef4a0f54df35334d
kia <dont@email.com>
2021-11-15 08:34:17 -0500

    start ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Provenance from './artifacts/contracts/Provencance.sol/Provencance.json'
+
+const ProvenanceAddress = '';
 
 function App() {
+  const [userAccount, setUserAccount] = useState();
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  async function registerOwnership() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(ProvenanceAddress, Provenance.abi, signer);
+      // const transation = await contract.transfer(userAccount, amount);
+      // await transation.wait();
+      // console.log(`${amount} successfully registered to ${userAccount}`);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
