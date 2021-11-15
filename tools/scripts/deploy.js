ea5ef45652c7895a8390a054ba93b90db92e943b
kia <dont@email.com>
2021-11-15 08:18:45 -0500

    sample-script -> deploy

--- tools/scripts/deploy.js
+++ tools/scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Provenance = await hre.ethers.getContractFactory("Provenance");
+  const provenance = await Provenance.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  await Provenance.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Provenance deployed to:", provenance.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
