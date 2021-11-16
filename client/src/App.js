096a4f08fdc3d0de28bd08d38f15d400d61366cd
kiacolbert <colber16@gmail.com>
2021-11-16 14:41:47 -0500

    contracts compile

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import { ethers } from 'ethers';
-import Provenance from './artifacts/contracts/Provencance.sol/Provencance.json'
+// import Provenance from './artifacts/contracts/Provenance.sol/Provenance.json'
 
 const ProvenanceAddress = '';
 
@@ -12,12 +12,12 @@ function App() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
   }
 
-  async function registerOwnership() {
+  async function registerArtist() {
     if (typeof window.ethereum !== 'undefined') {
       await requestAccount()
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(ProvenanceAddress, Provenance.abi, signer);
+      // const contract = new ethers.Contract(ProvenanceAddress, Provenance.abi, signer);
       // const transation = await contract.transfer(userAccount, amount);
       // await transation.wait();
       // console.log(`${amount} successfully registered to ${userAccount}`);
@@ -27,17 +27,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" onSubmit={registerArtist}/>
+      </form>
       </header>
     </div>
   );
